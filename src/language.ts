04a3b4d56f6f39179ec62b9231f25d29c727db26
Holger Stitz <holger.stitz@jku.at>
2018-03-23 18:02:50 +0100

    Rename application

--- src/language.ts
+++ src/language.ts
@@ -2,5 +2,4 @@
  * Created by Holger Stitz on Tue, 13 Mar 2018 10:20:09 GMT.
  */
 
-export const APP_NAME = 'Vega CLUE';
-export const HELLO_WORLD = 'Hello Phovea World';
+export const APP_NAME = 'Vega Gapminder';
