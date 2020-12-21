6356607a4cf495a59ab8f9abd182c7bc250178e7
Holger Stitz <holger.stitz@datavisyn.io>
2020-12-21 17:17:57 +0100

    Remove SCSS imports in TS files

--- dist/initialize.js
+++ dist/initialize.js
@@ -5,8 +5,6 @@ import 'vega_clue/dist/templates/404.html';
 import 'vega_clue/dist/robots.txt';
 import { AppWrapper } from './AppWrapper';
 import { APP_NAME } from './language';
-// import styles as last
-import 'vega_clue/dist/scss/main.scss';
 const app = new AppWrapper({
     prefix: 'vega',
     name: APP_NAME,
