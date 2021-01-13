25a077444b749359ee12bba6f8a3e309fef4cfc8
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2021-01-13 12:39:46 +0100

    remove imports

--- dist/initialize.d.ts
+++ dist/initialize.d.ts
@@ -1,5 +1,1 @@
-/**
- * Created by Holger Stitz on 13.03.2018.
- */
-import 'vega_clue/dist/templates/404.html';
-import 'vega_clue/dist/robots.txt';
+export {};
--- dist/initialize.js
+++ dist/initialize.js
@@ -1,8 +1,6 @@
 /**
  * Created by Holger Stitz on 13.03.2018.
  */
-import 'vega_clue/dist/templates/404.html';
-import 'vega_clue/dist/robots.txt';
 import { AppWrapper } from './AppWrapper';
 import { APP_NAME } from './language';
 const app = new AppWrapper({
